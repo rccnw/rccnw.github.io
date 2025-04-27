ef7a3701fb9ab7800308b0f90cfba37d9af85765
rccnw <rcc@pobox.com>
2025-04-27 14:26:48 -0700

    agent - theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rccnw.github.io',
   // No 'base' property is needed for deploying to the root user/org site
+  integrations: [tailwind()]
 });
